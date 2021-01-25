8322e7d3964cd9c51f9c7de6a006a9eff673046b
fbl <fbl0110>
2021-01-25 10:14:19 +0800

    m

--- controller/ArtController.js
+++ controller/ArtController.js
@@ -1,19 +1,18 @@
 // 文章控制器
+const fs = require('fs');
 let ArtController = {}
 
-// // 导入模拟（mock）的假数据
-// let articleData = require("../mockData/article.json");
+// 导入模拟（mock）的假数据
+let articleData = require("../mockData/article.json");
 
 // 导入model,相当于模型执行sql语句，
 const model = require('../model/model.js');
 
 // 导入返回结果的信息
-const { delsucc, delfail, exception, argsfail, addsucc, addfail, getsucc, getfail, updsucc, updfail } = require('../util/Exception_xx.js');
+const { delsucc, delfail, exception, argsfail, addsucc, addfail, getsucc, getfail, updsucc, updfail } = require('../util/responseMessage.js');
 
-//文章页面
-ArtController.artindex = (req, res) => {
-    res.render('article-index.html')
-}
+
+
 
 // 获取分页的文章数据
 ArtController.allArticle = async(req, res) => {
@@ -21,7 +20,9 @@ ArtController.allArticle = async(req, res) => {
     let { page, limit: pagesize } = req.query;
     //2.编写sql语句
     let offset = (page - 1) * pagesize;
-    let sql = `select * from article order by art_id desc limit ${offset},${pagesize} `;
+    let sql = `select t1.*,t2.name from article t1 left join category t2 on t1.cat_id = t2.cat_id
+    
+                order by t1.art_id desc limit ${offset},${pagesize} `;
     let sql2 = `select count(*) as count from article;`
     let promise1 = model(sql); // [{},{},{}]
     let promise2 = model(sql2); // [{count:16}]
@@ -64,11 +65,11 @@ ArtController.artAdd = (req, res) => {
     res.render('article-add.html')
 }
 
-提交数据入库
+// 提交数据入库
 ArtController.postArt = async(req, res) => {
-    let { title, cat_id, status, content } = req.body;
-    let sql = `insert into article(title,content,cat_id,status)
-                values('${title}','${content}',${cat_id},${status})
+    let { title, cat_id, status, content, cover } = req.body;
+    let sql = `insert into article(title,content,author,cat_id,status,cover,publish_date)
+                values('${title}','${content}','admin',${cat_id},${status},'${cover}',now())
                 `;
     let result = await model(sql)
     if (result.affectedRows) {
@@ -78,6 +79,35 @@ ArtController.postArt = async(req, res) => {
     }
 
 }
+
+//实现文件上传
+ArtController.upload = (req, res) => {
+    console.log(req.file); //接收文件上传成功后的信息
+    if (req.file) {
+        // 进行文件的重命名即可 fs.rename
+        let { originalname, destination, filename } = req.file;
+        let dotIndex = originalname.lastIndexOf('.');
+        let ext = originalname.substring(dotIndex);
+        let oldPath = `${destination}${filename}`;
+        let newPath = `${destination}${filename}${ext}`;
+        fs.rename(oldPath, newPath, err => {
+            if (err) { throw err; }
+            res.json({ message: '上传成功', code: 0, src: newPath })
+        })
+    } else {
+        res.json({ message: '上传失败', code: 1, src: '' })
+    }
+}
+
+// 获取单条文章
+ArtController.getOneArt = async(req, res) => {
+    let { art_id } = req.query;
+    let sql = `select * from article where art_id = ${art_id}`;
+    let data = await model(sql); // [{}]
+    res.json(data[0] || {})
+
+}
+
 
 // 暴露模块
 module.exports = ArtController;
--- model/model.js
+++ model/model.js
@@ -1,15 +1,17 @@
+// 封装执行sql语句的promise函数
 var mysql = require('mysql');
 
 // 导入数据库的配置参数
 let dbConfig = require("../config/my_sql.json");
 
 
+//连接数据库参数配置
 var connection = mysql.createConnection({
-    // 参数展开
+    // 展开参数
     ...dbConfig
 });
 
-
+//连接mysql
 connection.connect(function(err) {
     if (err) {
         throw err;
@@ -22,9 +24,12 @@ function dbquery(sql) {
     return new Promise((resolve, reject) => {
         connection.query(sql, (err, data) => {
             if (err) { reject(err); }
+            // select(查) => data是一个数组[{},{},..]  data.length > 说明有数据
+            // insert delete update（增删改） => data是一个对象 data.affectedRows > 0说明成功
             resolve(data)
         })
     })
 }
 
+// 暴露模块的内容
 module.exports = dbquery;
